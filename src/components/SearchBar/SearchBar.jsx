2bebc0fc56bf943d63f54c648b7e628c4fd25764
Velykobrat <149200949+Velykobrat@users.noreply.github.com>
2024-06-11 13:18:27 +0300

    SearchBar Component
    додано метод toast для відображенння помилка при порожньому полі

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './SearchBar.module.css';
 import { FaSearch } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SearchBar = ({ onSubmit }) => {
     const [query, setQuery] = useState('');
@@ -13,7 +15,7 @@ const SearchBar = ({ onSubmit }) => {
     const handleFormSubmit = (e) => {
         e.preventDefault(); // Запобігаємо стандартній поведінці форми
         if (query.trim() === '') {
-            alert('Please enter a search term');
+            toast.error('Please enter a search term');
             return;
         }
         onSubmit(query);
@@ -36,6 +38,7 @@ const SearchBar = ({ onSubmit }) => {
                     <FaSearch />
                 </button>
             </form>
+            <ToastContainer />
         </header>
     );
 };
